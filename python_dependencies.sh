1c73b676f62d2dc0e1b07f487b6ccb49c33b9ec3
cheonghong <ch.dev7@gmail.com>
2019-03-08 20:04:19 +0800

    Install Python dependencies for Linux

---
+++ python_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+pip install --upgrade setuptools
+pip install flask 
+pip install flask-cors
+pip install requests
+pip install durable_rules
+
+
+echo "dependencies installation complete"
