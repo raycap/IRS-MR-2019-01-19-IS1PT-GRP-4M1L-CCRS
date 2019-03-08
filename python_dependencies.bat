b001d786ed7431107537b4c6a69f42375ec098fe
cheonghong <ch.dev7@gmail.com>
2019-03-08 20:03:46 +0800

    Install Python dependencies for Windows

---
+++ python_dependencies.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+pip install --upgrade setuptools
+pip install flask 
+pip install flask-cors
+pip install requests
+pip install durable_rules
+
+ECHO "dependencies installation complete"
+
+PAUSE
